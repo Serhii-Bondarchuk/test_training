151a70d86506bf028f111d288ed4cba3b60f9a9e
Serhii Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2024-03-29 02:36:16 +0200

    added data

--- src/components/DataLoad/DataLoad.tsx
+++ src/components/DataLoad/DataLoad.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type PostsType = {
+  userId: 1
+  id: 1
+  title: string
+  body: string
+}
 
 function DataLoad() {
-  return <div>DataLoad from server!</div>
+  const [posts, setPosts] = useState<PostsType[]>()
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts?_limit=10'
+      )
+      console.log(response.data)
+      setPosts(response.data)
+    }
+    fetchData()
+  }, [])
+  return (
+    <>
+      <div>DataLoad from server!</div>
+      {posts?.map((post) => (
+        <>
+          <p key={post.id}>
+            {post.id}. {post.title}
+          </p>
+        </>
+      ))}
+    </>
+  )
 }
 
 export default DataLoad
